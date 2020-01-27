1e8b49109bde38814abb4fb700e7bac1534851e3
NoahMalhi <malhin@bu.edu>
2020-01-27 11:20:15 -0500

    Fixing testing error

--- test_file.py
+++ test_file.py
@@ -2,6 +2,6 @@ import arab_to_rom
 import pytest
 
 def test_awnser():
-    y = ""
-    x, y = arab_to_rom.conv_to_rom(4, y)
+    arab_to_rom.input = lambda: '4'
+    output = arab_to_rom.conv_to_rom(x, y)
     assert y == "IV"
