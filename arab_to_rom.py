a73a4a2343ec76eefc92e3da3d83f1498d44bede
NoahMalhi <malhin@bu.edu>
2020-01-27 09:37:54 -0500

    Converted code to store result into a string. Added bounds to inputable numbers from 0 to 9999.

--- arab_to_rom.py
+++ arab_to_rom.py
@@ -1,53 +1,64 @@
-def conv_to_rom(arab_in):
-    if arab_in >= 1000:
-        print('M', end = '')
-        return (arab_in - 1000)
+
+def conv_to_rom(arab_in, endconv):
+
+   
+    if arab_in >= 5000:
+        endconv = endconv + '(V)'
+        return (arab_in - 5000), endconv
+    elif arab_in >= 1000:
+        endconv = endconv + 'M'
+        return (arab_in - 1000), endconv
     elif arab_in >= 900:
-        print('CM', end = '')
-        return (arab_in - 900)
+        endconv = endconv + 'CM'
+        return (arab_in - 900), endconv
     elif arab_in >= 500:
-        print('D', end = '')
-        return (arab_in - 500)
+        endconv = endconv + 'D'
+        return (arab_in - 500), endconv
     elif arab_in >= 400:
-        print('CD', end = '')
-        return (arab_in - 400)
+        endconv = endconv + 'CD'
+        return (arab_in - 400), endconv
     elif arab_in >= 100:
-        print('C', end = '')
-        return (arab_in - 100)
+        endconv = endconv + 'C'
+        return (arab_in - 100), endconv
     elif arab_in >= 90:
-        print('XC', end = '')
-        return (arab_in - 90)   
+        endconv = endconv + 'XC'
+        return (arab_in - 90)  , endconv 
     elif arab_in >= 50:
-        print('L')
-        return (arab_in - 50)
+        endconv = endconv + 'L'
+        return (arab_in - 50), endconv
     elif arab_in >= 40:
-        print('XL', end = '')
-        return (arab_in - 40)
+        endconv = endconv + 'XL'
+        return (arab_in - 40), endconv
     elif arab_in >= 10:
-        print('X', end = '')
-        return (arab_in - 10)
+        endconv = endconv + 'X'
+        return (arab_in - 10), endconv
     elif arab_in >= 9:
-        print('IX', end = '')
-        return (arab_in - 9)
+        endconv = endconv + 'IX'
+        return (arab_in - 9), endconv
     elif arab_in >= 5:
-        print('V', end = '')
-        return (arab_in - 5)
+        endconv = endconv + 'V'
+        return (arab_in - 5), endconv
     elif arab_in == 4:
-        print('IV', end = '')
-        return (arab_in - 4)
+        endconv = endconv + 'IV'
+        return (arab_in - 4), endconv
     elif arab_in >= 1:  
-        print('I', end = '')
-        return (arab_in - 1)
+        endconv = endconv + 'I'
+        return (arab_in - 1), endconv
     else:
         return 0
 
 arab_in = int(input("Enter a number : "))
+endconv = ""
 for x in range(arab_in):
-    arab_in = conv_to_rom(arab_in)
-    if arab_in == 0:
+    if arab_in > 9999:
+        print("Entered value must be less than 10,000")
         break
-    
-print('')
+    arab_in, endconv = conv_to_rom(arab_in, endconv)
+    if arab_in == 0 :
+        break
+
 if arab_in < 0:
-        print ("Enter value greater than 0")
+    print ("Enter value greater than 0")   
+print(endconv)
 print('Done')
+
